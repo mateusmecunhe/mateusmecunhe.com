4e310eb03cfa1ca666271fc448d26a3fe46b5ae2
mateus <mateusmecunhe@gmail.com>
2019-09-13 15:13:12 -0300

    implementing more style

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 const hero = document.querySelector('.hero');
 const slider = document.querySelector('.slider');
 const menu = document.querySelector('.menu');
+const presentation = document.querySelector('.presentation');
 
 const tl = new TimelineMax();
 
 
 console.log(hero);
 
-tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
+tl.fromTo(hero, 0.5, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
 .fromTo(hero, 1.2, {width: '100%'}, {width: "80%", ease: Power2.easeInOut})
-.fromTo(menu, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2");
+.fromTo(menu, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2").
+fromTo(presentation, 0.0, {z: "-5", opacity: 0}, {z: "1", opacity: 1}, "-=1.2");
---
+++ js/menu.js
@@ -0,0 +1,6 @@
+
+
+var btn = document.querySelector('#nav-icon');
+btn.addEventListener('click', function(){
+    this.classList.toggle('open');
+})
